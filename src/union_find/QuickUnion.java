683d1f33c5766383c2711bcf403da276f524318e
Andrii Krymets <gromozzzeka@hotmail.com>
2015-09-08 20:42:12 +0300

    Initial project commit

---
+++ src/union_find/QuickUnion.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package union_find;
+
+/**
+ *
+ * @author Andrey
+ */
+public class QuickUnion {
+
+    int[] array = new int[10];
+
+    public QuickUnion() {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+    }
+
+    void union(int p, int q) {
+
+        for (int i = 0; i < this.array.length; i++) {
+            if (this.array[i] = ) {
+                
+            }
+            
+
+        }
+
+    }
+
+}
