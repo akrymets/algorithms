9739313a0dffc3e192d25b378eb952c3a3bc2416
akrymets <andrii.krymets@outlook.com>
2015-09-13 19:59:43 +0300

    isFull() and connected() methods are working.
    Corrected indexes of the mapped array.

--- src/week1pa/Percolation.java
+++ src/week1pa/Percolation.java
@@ -27,6 +27,7 @@ public class Percolation {
 
         openedSites = new boolean[N * N + 1];
 
+        
         for (int i = 1; i <= N * N; i++) {
             openedSites[i] = false;
         }
@@ -38,36 +39,31 @@ public class Percolation {
     public void open(int i, int j) {
         checkXYValidity(i, j);
 
-        // Return if already open
+        // Finishes if already open
         if (isOpen(i, j)) {
             return;
         }
 
-        int site = xyTo1D(i, j);
+        int site1DIndex = xyTo1D(i, j);
 
         // Mark this site as open
-        openedSites[site] = true;
+        openedSites[site1DIndex] = true;
 
         // If the site is in the first row it must be also union with the virtual 0-site;
-        if (site <= N) {
-            qu.union(0, site);
-        }
-
-        // If the site is in the last row it must be also union with the virtual N * N + 1 -site;
-        if (site <= N * N && site > N * (N - 1) + 1) {
-            qu.union(N * N, site);
+        if (site1DIndex <= N - 1) {
+            qu.union(N, site1DIndex); // the virtual 0-site has index N in the array
         }
 
         int[] neighbors = getNeighbors(i, j);
 
         for (int k = 0; k < neighbors.length; k++) {
-            if (neighbors[k] != -1) {
+            if (neighbors[k] != -1) { // means this neighbor is out of grid
                 int x = xyFrom1D(neighbors[k])[0];
                 int y = xyFrom1D(neighbors[k])[1];
 
-            // if a neighbor site is open and is not out bounds we union it with the site
+            // if a neighbor site is open and is not out bounds we union it with the site we have just opened
                 if (isOpen(x, y)) {
-                    qu.union(neighbors[k], site);
+                    qu.union(neighbors[k], site1DIndex);
                 }
             }
         }
@@ -75,14 +71,12 @@ public class Percolation {
 
     public boolean isOpen(int i, int j) {
         checkXYValidity(i, j);
-
-        return openedSites[xyTo1D(j, j)];
+        return openedSites[xyTo1D(i, j)];
     }
 
     public boolean isFull(int i, int j) {
         checkXYValidity(i, j);
-
-        return false;
+        return qu.connected(xyTo1D(i, j), N); // the virtual 0-site has index N in the array
 
     }
 
@@ -99,22 +93,38 @@ public class Percolation {
 
         int[] neighbors = new int[4];
 
-        neighbors[0] = xyTo1D(x, y - 1);
-        neighbors[1] = xyTo1D(x + 1, y);
-        neighbors[2] = xyTo1D(x, y + 1);
-        neighbors[3] = xyTo1D(x - 1, y);
+        // If neighbor's coordinates are not valid - the xyTo1D function returns -1
+        neighbors[0] = xyTo1D(x, y - 1); // Neighbor on top
+        neighbors[1] = xyTo1D(x + 1, y); // Neighbor on right
+        neighbors[2] = xyTo1D(x, y + 1); // Neighbor on bottom
+        neighbors[3] = xyTo1D(x - 1, y); // Neighbor on left
 
         return neighbors;
     }
 
+    /**
+     * Map site's coordinates in the grid into the its array index.
+     * @param x
+     * @param y
+     * @return Returns mapped site's number in the array or -1 if the coordinates are not valid
+     */
     private int xyTo1D(int x, int y) {
-        return xyAreValid(x, y) ? N * (y - 1) + x : -1;
+        return xyAreValid(x, y) ? N * (y - 1) + x - 1 : -1;
     }
 
+    /**
+     * Map site's index in the array into its coordinates in the grid
+     * @param m
+     * @return Array of two site's coordinates in the grid: xy[0] - x; xy[1] - y;
+     */
     private int[] xyFrom1D(int m) {
+        if (m < 0 || m > N * N - 1) {
+            throw new IllegalArgumentException("site's id is out of bonds of array");
+        }
+        
         int[] xy = new int[2];
-        xy[0] = m % N;
-        xy[1] = m / N + 1;
+        xy[0] = (m + 1) % N != 0 ? (m + 1) % N : N;
+        xy[1] = (m + 1) % N != 0 ? (m + 1) / N + 1 : (m + 1) / N;
         return xy;
     }
 
@@ -140,18 +150,32 @@ public class Percolation {
      */
     public static void main(String[] args) {
 
-        Percolation perc = new Percolation(10);
+        Percolation p = new Percolation(5);
 
-        perc.open(3, 1);
-        perc.open(4, 4);
-        perc.open(2, 3);
-        perc.open(3, 2);
+        p.open(3, 1);
+        System.out.println("isFull: " + p.isFull(2, 1)); // false
+        System.out.println("isFull: " + p.isFull(3, 1)); // true
+        System.out.println("------------------");
         
-        System.out.println(perc.connected(perc.xyTo1D(3, 1), perc.xyTo1D(3, 2)));
+        p.open(3, 2);
+        System.out.println(p.connected(p.xyTo1D(3, 2), p.xyTo1D(3, 1))); // true
+        System.out.println("isFull: " + p.isFull(3, 2)); // true
+        System.out.println(p.connected(p.xyTo1D(3, 2), p.xyTo1D(2, 2))); // false
+        System.out.println(p.connected(p.xyTo1D(4, 2), p.xyTo1D(3, 1))); // false
+        System.out.println("isFull: " + p.isFull(4, 2)); // false
+        System.out.println("isFull: " + p.isFull(3, 3)); // false
+        System.out.println("isFull: " + p.isFull(5, 5)); // false
+        System.out.println("------------------");
         
-        perc.open(2, 2);
+        p.open(4, 2);
+        System.out.println(p.connected(p.xyTo1D(4, 2), p.xyTo1D(3, 1))); // true
+        System.out.println("isFull: " + p.isFull(4, 2)); // true
+        System.out.println("isFull: " + p.isFull(4, 4)); // false
+        System.out.println("isFull: " + p.isFull(1, 5)); // false
+        System.out.println("isFull: " + p.isFull(1, 1)); // false
         
-        System.out.println(perc.connected(perc.xyTo1D(2, 3), perc.xyTo1D(2, 2)));
+//        int[] test = p.xyFrom1D(5);
+//        System.out.println("x: " + test[0] + ", y: " + test[1]);
         
         
     }
