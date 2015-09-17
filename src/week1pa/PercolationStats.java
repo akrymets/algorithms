fdbb9fdb45621d06da1d53fd59ab313cb7d1fdf3
akrymets <andrii.krymets@outlook.com>
2015-09-17 14:16:29 +0300

    PercolationStats class updated

--- src/week1pa/PercolationStats.java
+++ src/week1pa/PercolationStats.java
@@ -6,6 +6,8 @@
 package week1pa;
 
 import edu.princeton.cs.algs4.QuickFindUF;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdRandom;
 
 /**
  *
@@ -13,7 +15,14 @@ import edu.princeton.cs.algs4.QuickFindUF;
  */
 public class PercolationStats {
 
+    private double[] tresholds;
     QuickFindUF qf;
+    Percolation p;
+    private final int T;
+    private final int N;
+    private double mean;
+    private double stddev;
+    
     
     /**
      * perform T independent experiments on an N-by-N grid
@@ -23,17 +32,50 @@ public class PercolationStats {
     public PercolationStats(int N, int T) {
         if (T <= 0 || N <= 0) throw new java.lang.IllegalArgumentException();
         
+        this.T = T;
+        this.N = N;
+        
         qf = new QuickFindUF(N);
         
+        tresholds = new double[T];
+        
+        for (double treshold : tresholds) {
+            treshold = 0.0;
+        }
         
     }
 
+    /**
+     * Returns percolation treshold for a single test
+     * @return 
+     */
+    private double treshold(){
+        p = new Percolation(N);
+        
+        int openedSitesUntilPercolates = 0;
+        while (!p.percolates()){
+            openedSitesUntilPercolates++;
+            p.open(StdRandom.uniform(1, N + 1), StdRandom.uniform(1, N + 1));
+        }
+        return 1.0 * openedSitesUntilPercolates / (N * N);
+    }
+    
+    /**
+     * run T tests and fill the tresholds[] array with tresholds for each tests
+     */
+    private void runTests(){
+        for (int i = 0; i < T; i++) {
+            tresholds[i] = treshold();
+        }
+    }
+    
     /**
      * sample mean of percolation threshold
      * @return 
      */
     public double mean() {
-        return 0;
+        mean = StdStats.mean(tresholds);
+        return mean;
     }
     
     /**
@@ -41,7 +83,8 @@ public class PercolationStats {
      * @return 
      */
     public double stddev(){
-        return 0;
+        stddev = StdStats.stddev(tresholds);
+        return stddev;
     }
 
     /**
@@ -49,7 +92,7 @@ public class PercolationStats {
      * @return 
      */
     public double confidenceLo(){
-        return 0;
+        return mean - (1.96 * stddev / (Math.sqrt(T)));
     }
     
     /**
@@ -57,11 +100,26 @@ public class PercolationStats {
      * @return 
      */
     public double confidenceHi(){
-        return 0;
+        return mean + (1.96 * stddev / (Math.sqrt(T)));
+    }
+    
+    private void showTestsResults(){
+        double ciLo = confidenceLo();
+        double ciHi = confidenceHi();
+        double mean = mean();
+        double stddev = stddev();
+        
+        System.out.println(mean);
+        System.out.println(stddev);
+        System.out.println(ciLo);
+        System.out.println(ciHi);
     }
     
     public static void main(String[] args) {
         
+        PercolationStats ps = new PercolationStats(100, 30);
+        
+        ps.showTestsResults();
     }
     
 }
