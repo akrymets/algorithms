b47bbf4705e197caf67fd95da62843c6b6ad2793
akrymets <andrii.krymets@outlook.com>
2015-09-19 21:43:34 +0300

    code styling of .java files has been improved

--- src/week1pa/PercolationStats.java
+++ src/week1pa/PercolationStats.java
@@ -10,23 +10,40 @@
  * T - number of tests
  * 
  * Example:
- * project_root> java -cp /build/classes;lib/algs4.jar week1pa.PercolationStats 100 200
+ * java -cp /build/classes;lib/algs4.jar week1pa.PercolationStats 100 200
  */
-package week1pa;
 
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-    private double[] fractionsOfOpenSites; // number of open sites fractions for each of T experiments
-    Percolation p; // Percolation class implements some needed methods for performing UnionFind operations
-    private static int T; // number of experiments
-    private static int N; // number of elements in the grid's dimensions
-    private double mean = 0.0; // sample mean of percolation threshold
-    private double stddev = 0.0; // sample standard deviation of percolation threshold
-    private double confidenceLo; // low  endpoint of 95% confidence interval
-    private double confidenceHi; //high endpoint of 95% confidence interval
+    // number of open sites fractions for each of T experiments
+    private double[] fractionsOfOpenSites;
+
+    /*
+     * Percolation class implements some needed methods for performing UnionFind
+     * operations
+     */
+    private Percolation p;
+
+    // number of experiments
+    private int t;
+
+    // number of elements in the grid's dimensions
+    private int n;
+
+    // sample mean of percolation threshold
+    private double mean = 0.0;
+
+    // sample standard deviation of percolation threshold
+    private double stddev = 0.0;
+
+    // low  endpoint of 95% confidence interval
+    private double confidenceLo;
+
+    // high endpoint of 95% confidence interval
+    private double confidenceHi;
 
     /**
      * perform T independent experiments on an N-by-N grid
@@ -39,21 +56,12 @@ public class PercolationStats {
             throw new java.lang.IllegalArgumentException();
         }
 
-        PercolationStats.N = N;
-        PercolationStats.T = T;
+        this.n = N;
+        this.t = T;
 
         fractionsOfOpenSites = new double[T];
 
-        for (double fraction : fractionsOfOpenSites) {
-            fraction = 0.0;
-        }
-
         runExperiments();
-
-        this.mean = mean();
-        this.stddev = stddev();
-        this.confidenceLo = confidenceLo();
-        this.confidenceHi = confidenceHi();
 
     }
 
@@ -61,22 +69,31 @@ public class PercolationStats {
      * Method runs T experiments and fills the fractionsOfOpenSites array
      */
     private void runExperiments() {
-        for (int i = 0; i < PercolationStats.T; i++) {
+        for (int i = 0; i < this.t; i++) {
 
-            int count = 0; // stores fraction of open sites when percolates for current experiment
+            /*
+             * stores fraction of open sites when percolates for current
+             * experiment open_sites / sqr(N)
+             */
+            int count = 0;
 
-            p = new Percolation(N);
+            p = new Percolation(n);
 
             while (!p.percolates()) {
-                int row = StdRandom.uniform(1, PercolationStats.N + 1);
-                int column = StdRandom.uniform(1, PercolationStats.N + 1);
+                int row = StdRandom.uniform(1, this.n + 1);
+                int column = StdRandom.uniform(1, this.n + 1);
                 if (!p.isOpen(row, column)) {
                     p.open(row, column);
                     count++;
                 }
             }
-            fractionsOfOpenSites[i] = count / Math.pow(PercolationStats.N, 2);
+            fractionsOfOpenSites[i] = count / (this.n * this.n);
         }
+
+        this.mean = mean();
+        this.stddev = stddev();
+        this.confidenceLo = confidenceLo();
+        this.confidenceHi = confidenceHi();
     }
 
     /**
@@ -103,7 +120,7 @@ public class PercolationStats {
      * @return
      */
     public double confidenceLo() {
-        return this.mean - (1.96 * this.stddev / (Math.sqrt(PercolationStats.T)));
+        return mean - (1.96 * stddev / (Math.sqrt(this.t)));
     }
 
     /**
@@ -112,7 +129,7 @@ public class PercolationStats {
      * @return
      */
     public double confidenceHi() {
-        return mean + (1.96 * stddev / (Math.sqrt(PercolationStats.T)));
+        return mean + (1.96 * stddev / (Math.sqrt(this.t)));
     }
 
     /**
@@ -121,13 +138,17 @@ public class PercolationStats {
     private void printResult() {
         System.out.println("mean\t\t\t\t= " + this.mean);
         System.out.println("stddev\t\t\t\t= " + this.stddev);
-        System.out.println("95% confidence interval\t\t= " + this.confidenceLo + ", " + this.confidenceHi);
+        System.out.println("95% confidence interval\t\t= " + this.confidenceLo
+                + ", " + this.confidenceHi);
         System.out.println();
     }
 
     public static void main(String[] args) {
 
-        PercolationStats ps = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        PercolationStats ps = new PercolationStats(
+                Integer.parseInt(args[0]),
+                Integer.parseInt(args[1]));
+
         ps.printResult();
 
     }
